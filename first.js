90deecf80ab5915935852746916433d2ca5b7e8f
charank23 <charan5184819@gmail.com>
2025-08-08 23:37:56 +0530

    second commit

--- first.js
+++ first.js
@@ -1,2 +1,42 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
+//header 
+//body 
+//footer 
+function Card(){
+    return (
+        <div style={{border:"2px solid green",padding:"2px"}}>
+            <img src="https://www.pngkey.com/png/full/72-723644_blank-black-t-shirt-png.png" height="200px" width="calc(20% - 20px)"/>
+            <div style={{textAlign:"center"}}>
+                <h2>T-shirt</h2>
+                <h1>40-60% off</h1>
+                <h2>SHOP NOW</h2>
+            </div>
+        </div>
+    )
+
+}
+function App(){
+    return (
+        //header
+        //body
+        <div style={{display:"flex",flexWrap: "wrap",gap:"20px"}}>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/> 
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
